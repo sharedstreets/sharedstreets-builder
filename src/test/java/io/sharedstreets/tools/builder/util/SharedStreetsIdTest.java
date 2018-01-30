f398d0bcf37b91d5285958509d024c55e9508ea9
Kevin Webb <kpwebb@gmail.com>
2018-01-29 21:45:03 -0500

    update tests

--- src/test/java/io/sharedstreets/tools/builder/util/SharedStreetsIdTest.java
+++ src/test/java/io/sharedstreets/tools/builder/util/SharedStreetsIdTest.java
@@ -4,43 +4,39 @@ import com.esri.core.geometry.Point;
 import io.sharedstreets.data.SharedStreetsIntersection;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
 
 
-public class UniqueIdTest {
+public class SharedStreetsIdTest {
 
     @Test
-    public void evaluate() {
+    public void evaluate() throws Exception {
 
-        // hash test from https://github.com/sharedstreets/sharedstreets-ref-system/issues/8
-        assertEquals(UniqueId.generateHash("Intersection 110.000000 45.000000").toString(), "71f34691f182a467137b3d37265cb3b6");
-        assertEquals(UniqueId.generateHash("Intersection -74.003388 40.634538").toString(), "103c2dbe16d28cdcdcd5e5e253eaa026");
-        assertEquals(UniqueId.generateHash("Intersection -74.004107 40.634060").toString(), "0f346cb98b5d8f0500e167cb0a390266");
-        assertEquals(UniqueId.generateHash("Intersection -74.009094 40.726002").toString(), "6f1c08066370129f2f429f3c20f9e151");
-
-
-        // test decoding/encoding of hash string
-        UniqueId uniqueId1 = UniqueId.generateHash("Intersection 110.000000 45.000000");
-
-        String idStr = uniqueId1.toString();
-
-        UniqueId uniqueId2 = null;
-        try {
-            uniqueId2 = UniqueId.fromString(idStr);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        assertEquals(uniqueId1, uniqueId2);
-
+        // test ID generation for sharedstreets data types
+        // key is lon,lat (x,y) ordering for hash strings and
 
         SharedStreetsIntersection sharedStreetsIntersection = new SharedStreetsIntersection();
 
         sharedStreetsIntersection.point = new Point();
-        sharedStreetsIntersection.point.setX(-74.00909423828125);
-        sharedStreetsIntersection.point.setY(40.72600173950195);
+        sharedStreetsIntersection.osmNodeId = 123l;
+        sharedStreetsIntersection.inboundSegmentIds = new UniqueId[0];
+        sharedStreetsIntersection.outboundSegmentIds = new UniqueId[0];
 
-        assertEquals(sharedStreetsIntersection.getId(), "6f1c08066370129f2f429f3c20f9e151");
+        sharedStreetsIntersection.point.setX(-74.0090917); // longitude -> float precision becomes -74.009094
+        sharedStreetsIntersection.point.setY(40.7260025); // latitude
+
+        // should generate message Intersection -74.009094 40.726002
+        assertEquals(UniqueId.generateHash("Intersection -74.009092 40.726003").toString(), "8037a9444353cd7dd3f58d9a436f2537");
+
+        assertEquals(sharedStreetsIntersection.getId(), "8037a9444353cd7dd3f58d9a436f2537");
+
+        byte[] data = sharedStreetsIntersection.toBinary();
+
+        SharedStreetsIntersection sharedStreetsIntersection2 = SharedStreetsIntersection.fromBinary(data);
+
+        assertEquals(sharedStreetsIntersection.point.getX(), sharedStreetsIntersection2.point.getX(), 0.000001);
 
 
     }
--- src/test/java/io/sharedstreets/tools/builder/util/UniqueIdTest.java
+++ src/test/java/io/sharedstreets/tools/builder/util/UniqueIdTest.java
@@ -1,5 +1,7 @@
 package io.sharedstreets.tools.builder.util;
 
+import com.esri.core.geometry.Point;
+import io.sharedstreets.data.SharedStreetsIntersection;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -14,6 +16,8 @@ public class UniqueIdTest {
         assertEquals(UniqueId.generateHash("Intersection 110.000000 45.000000").toString(), "71f34691f182a467137b3d37265cb3b6");
         assertEquals(UniqueId.generateHash("Intersection -74.003388 40.634538").toString(), "103c2dbe16d28cdcdcd5e5e253eaa026");
         assertEquals(UniqueId.generateHash("Intersection -74.004107 40.634060").toString(), "0f346cb98b5d8f0500e167cb0a390266");
+        assertEquals(UniqueId.generateHash("Intersection -74.009094 40.726002").toString(), "6f1c08066370129f2f429f3c20f9e151");
+
 
         // test decoding/encoding of hash string
         UniqueId uniqueId1 = UniqueId.generateHash("Intersection 110.000000 45.000000");
@@ -28,6 +32,7 @@ public class UniqueIdTest {
         }
 
         assertEquals(uniqueId1, uniqueId2);
+
 
     }
 
